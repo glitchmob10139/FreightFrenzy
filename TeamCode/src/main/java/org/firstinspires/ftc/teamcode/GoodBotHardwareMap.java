e86f0458868c15b47ed003d164f33174436b6f45
glitchmob10139 <55032072+glitchmob10139@users.noreply.github.com>
2022-01-01 20:42:48 -0600

    added new code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodBotHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoodBotHardwareMap.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -35,22 +6,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/**
- * This is NOT an opmode.
- *
- * This class can be used to define all the specific hardware for a single robot.
- * In this case that robot is a Pushbot.
- * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
- *
- * This hardware class assumes the following device names have been configured on the robot:
- * Note:  All names are lower case and some have single spaces between words.
- *
- * Motor channel:  Left  drive motor:        "left_drive"
- * Motor channel:  Right drive motor:        "right_drive"
- * Motor channel:  Manipulator drive motor:  "left_arm"
- * Servo channel:  Servo to open left claw:  "left_hand"
- * Servo channel:  Servo to open right claw: "right_hand"
- */
+
 public class GoodBotHardwareMap
 {
     /* Public OpMode members. */
@@ -59,8 +15,7 @@ public class GoodBotHardwareMap
     public DcMotor  rightFront  = null;
     public DcMotor  rightRear    = null;
     public DcMotor  duckSpinner   = null;
-    public Servo primaryServo  = null;
-    public Servo secondaryServo  = null;
+    public void Servo clawServo  = null;
 
 
     /* local OpMode members. */
@@ -83,14 +38,9 @@ public class GoodBotHardwareMap
         rightFront    = hwMap.get(DcMotor.class, "right_front");
         rightRear   = hwMap.get(DcMotor.class, "right_rear");
         duckSpinner = hwMap.get(DcMotor.class, "duck_spinner");
-        mainArm1  = hwMap.get(Servo.class, "main_arm_1");
-        mainArm2  = hwMap.get(Servo.class, "main_arm_2");
-        slidingArm1  = hwMap.get(Servo.class, "sliding_arm_1");
-        slidingArm2  = hwMap.get(Servo.class, "sliding_arm_2");
-        dumpBucket  = hwMap.get(Servo.class, "dump_bucket");
 
 
-        //Setting directions.
+        //setting directions.
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         leftRear.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
@@ -113,12 +63,9 @@ public class GoodBotHardwareMap
 
 
         // Define and initialize ALL installed servos.
-        primaryServo = hwMap.get(Servo.class, "primary_servo");
-        secondaryServo = hwMap.get(Servo.class, "secondary_servo");
-       
-        //Set all servo to the zero position
-        primaryServo.setPosition(0);
-        secondaryServo.setPosition(0);
+        clawServo = hwMap.get(Servo.class, "claw_servo");
+
+        // Set all servo to the 0 position
+        clawServo.setPosition(0);
     }
  }
-
